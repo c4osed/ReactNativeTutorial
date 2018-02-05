7295efdadf016f7f35095d759553a215b3ef23f7
C4osed <c4osed@outlook.com>
2018-02-05 12:15:57 +0700

    done

--- App.js
+++ App.js
@@ -4,34 +4,47 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({   ios: 'Press Cmd+R to reload,\nCmd+D
+// or shake for dev menu',   android: 'Double tap R on your keyboard to
+// reload,\nShake or press menu button for dev men' +       'u' });
 
-export default class App extends Component<{}> {
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }, 1000);
+  }
+  render() {
+    return (
+      <Text
+        style={[
+        styles.counter, {
+          fontSize: this.props.size
+        }
+      ]}>
+        {this.props.name}
+        : {this.state.count}</Text>
+    );
+  }
+}
+
+export default class App extends Component < {} > {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Counter name="Counter 1" size={12}/>
+        <Counter name="Counter 2" size={16}/>
+        <Counter name="Counter 3" size={32}/>
+        <Counter name="Counter 4" size={48}/>
       </View>
     );
   }
@@ -42,16 +55,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 5
   },
+  counter: {
+    fontSize: 20,
+    color: 'blue'
+  }
 });
--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-# React Native Tutorial
-
-## Create Project
-```
-react-native init {Project Name}
-```
-
-## Run
-IOS
-```
-react-native run-ios
-```
-Android
-```
-react-native run-android
-```
+# Counter
